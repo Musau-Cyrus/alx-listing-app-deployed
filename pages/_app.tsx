836eee50aafceca81afcef6c807b16da57ebcc77
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-07-21 11:25:53 +0300

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Head>
+      <title>AIRBnB Clone Project</title>
+      <link rel="icon" href="/hotel.png" /> 
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   );
 }
